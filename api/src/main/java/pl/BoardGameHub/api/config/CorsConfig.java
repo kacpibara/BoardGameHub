68a866c83edba14f778e03730ca57d2839aa6e3a
kacpibara <kacperbarancewicz@gmail.com>
2026-03-25 21:54:43 +0100

    Fix CORS policy for Vercel

--- api/src/main/java/pl/BoardGameHub/api/config/CorsConfig.java
+++ api/src/main/java/pl/BoardGameHub/api/config/CorsConfig.java
@@ -11,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
